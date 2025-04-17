1d2dd77a711cebc53409da739bd8b495ba84881c
xOAviOx <avishuklacode@gmail.com>
2025-04-17 11:04:51 +0530

    Golbal Error handler

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const morgan = require('morgan');
+const globalErrorHandler = require('./controllers/errorController');
 
 //1) Middlewares
 const app = express();
@@ -14,17 +16,21 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello fromt the middleware😊');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours/', tourRouter);
 app.use('/api/v1/users/', userRouter);
-//4) Server
+
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
