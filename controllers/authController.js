3c5fcaa250b053d899f3cf23ea4b64bd67218074
xOAviOx <avishuklacode@gmail.com>
2025-04-19 18:58:13 +0530

    Complete Auth

---
+++ controllers/authController.js
@@ -0,0 +1,98 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../utils/catchAsync');
+const User = require('./../model/userModel');
+const AppError = require('../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+  });
+
+  const token = signToken(newUser._id);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1 CHeck if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  //2 check if user exists && password is correct
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  console.log(user);
+  //3 If everything ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1 getting token and check if its there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+  }
+  //2 verification of token
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  //3 check if user still exists
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exists.',
+        401
+      )
+    );
+  }
+  //4 check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please login again.', 401)
+    );
+  }
+  //grant access to protected routes
+  req.user = currentUser;
+  next();
+});
+
---
+++ model/userModel.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User must have a name.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'User must have a email.'],
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function (value) {
+        return validator.isEmail(value);
+      },
+      message: 'Please provide a valid email.',
+    },
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'User must have a password'],
+    minlength: [8, 'Password must be at least 8 characters long.'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'User must confirm the password.'],
+    //only works on create and save
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same.',
+    },
+  },
+  passwordChangedAt: Date,
+});
+
+userSchema.pre('save', async function (next) {
+  //Only run this function if password was modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password witj cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  //False means not changed
+  return false;
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
