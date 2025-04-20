36650364e5ae2554e2d11b0fc56678a406312f47
xOAviOx <avishuklacode@gmail.com>
2025-04-20 10:06:49 +0530

    Update Password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -178,3 +178,26 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //1 get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  //2 check if posted password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong,', 401));
+  }
+  //3 if so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  //User.findById will not work as intended
+  
+  //4 log user in, send jwt
+  const token = signToken(user._id);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+  });
+});
