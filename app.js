49f2b201e6391f0b4ea2a6781709129eb6323838
xOAviOx <avishuklacode@gmail.com>
2025-04-20 12:14:35 +0530

    Security

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const morgan = require('morgan');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 //1) Global Middlewares
 
 //development logging
@@ -30,6 +32,25 @@ app.use('/api', limiter);
 //body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+//data sanitization against nosql query injection
+app.use(mongoSanitize());
+
+//data sanitization against XSS
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 //serving static files
 app.use(express.static(`${__dirname}/public`));
 
