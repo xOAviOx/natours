c48db06ca8136073e40d6d0afa8cacd046e6b953
xOAviOx <avishuklacode@gmail.com>
2025-04-30 11:31:26 +0530

    Refactored the API controllers

--- model/tourModel.js
+++ model/tourModel.js
@@ -121,6 +121,8 @@ tourSchema.index({ price: 1, ratingsAverage: -1 });
 
 tourSchema.index({ slug: 1 });
 
+tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
@@ -170,11 +172,11 @@ tourSchema.post('/^find/', function (docs, next) {
 });
 
 //Aggregation middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
