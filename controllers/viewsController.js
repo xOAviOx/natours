d5624eef9f6b2b218c5bfd839ad6eda29111c2c9
xOAviOx <avishuklacode@gmail.com>
2025-05-03 20:44:13 +0530

    Pug templates- Tour detail page - 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,14 +8,25 @@ exports.getOverview = catchAsync(async (req, res) => {
 
   //3 Render that template using tour data from 1
 
-  res.status(200).render('overview',{
-    title:'All tours',
-    tours
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours,
   });
 });
 
 exports.getTourView = catchAsync(async (req, res) => {
+  //1 get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  //2 build template
+
+  //3 render template using the data from 1
+
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: tour.slug,
+    tour,
   });
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.get('/', getOverview);
 
-router.get('/tour', getTourView);
+router.get('/tour/:slug', getTourView);
 
 module.exports = router;
