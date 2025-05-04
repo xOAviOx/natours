5e07815ff4b39fb50506475ee59a820f3083e940
xOAviOx <avishuklacode@gmail.com>
2025-05-04 20:40:12 +0530

    Rendering error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require('../utils/appError');
+const AppError = require("../utils/appError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
@@ -14,41 +14,70 @@ const handleDuplicateFieldsDB = (err) => {
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
-  const message = `Invalid input data. ${errors.join('. ')}`;
+  const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
 const handleJWTError = () =>
-  new AppError('Invalid token. Please log in again!', 401);
+  new AppError("Invalid token. Please log in again!", 401);
 
 const handleJWTExpiredError = () =>
-  new AppError('Your token has expired! Please log in again.', 401);
+  new AppError("Your token has expired! Please log in again.", 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
-const sendErrorProd = (err, res) => {
-  //Operational,trusted error:send message to client
-  if (err.isOperational) {
+const sendErrorDev = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith("/api")) {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
     });
-    //Programming or other unknown error:don't leak error details
   } else {
-    //Log error
-    console.error('ERROR 🔴', err);
-    //Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!',
+    //Rendered website
+    res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message,
     });
+  }
+};
+
+const sendErrorProd = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith("/api")) {
+    //Operational,trusted error:send message to client
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+      //Programming or other unknown error:don't leak error details
+    } else {
+      //Log error
+      console.error("ERROR 🔴", err);
+      //Send generic message
+      res.status(500).json({
+        status: "error",
+        message: "Something went very wrong!",
+      });
+    }
+  } else {
+    //Rendered website
+    if (err.isOperational) {
+      res.status(err.statusCode).render("error", {
+        title: "Something went wrong!",
+        msg: err.message,
+      });
+      //Programming or other unknown error:don't leak error details
+    } else {
+      //Log error
+      console.error("ERROR 🔴", err);
+      //Send generic message
+      res.status(err.statusCode).render("error", {
+        title: "Something went wrong!",
+        msg: "Please try again later",
+      });
+    }
   }
 };
 
@@ -56,19 +85,19 @@ module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === "production") {
     let error = Object.create(err);
 
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
+    if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError();
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
-    sendErrorProd(error, res);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+    sendErrorProd(error, req, res);
   }
 };
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,6 @@
-const Tour = require('../model/tourModel');
-const catchAsync = require('../utils/catchAsync');
-
+const Tour = require("../model/tourModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 exports.getOverview = catchAsync(async (req, res) => {
   //1 Get tour data from coll;ection
   const tours = await Tour.find();
@@ -8,8 +8,8 @@ exports.getOverview = catchAsync(async (req, res) => {
 
   //3 Render that template using tour data from 1
 
-  res.status(200).render('overview', {
-    title: 'All tours',
+  res.status(200).render("overview", {
+    title: "All tours",
     tours,
   });
 });
@@ -17,15 +17,18 @@ exports.getOverview = catchAsync(async (req, res) => {
 exports.getTourView = catchAsync(async (req, res, next) => {
   //1 get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-    path: 'reviews',
-    fields: 'review rating user',
+    path: "reviews",
+    fields: "review rating user",
   });
 
+  if (!tour) {
+    return next(new AppError("There is no tour with that name.", 404));
+  }
   //2 build template
 
   //3 render template using the data from 1
 
-  res.status(200).render('tour', {
+  res.status(200).render("tour", {
     title: `${tour.name} Tour`,
     tour,
   });
@@ -35,10 +38,10 @@ exports.getLogin = async (req, res) => {
   res
     .status(200)
     .set(
-      'Content-Security-Policy',
+      "Content-Security-Policy",
       "connect-src 'self' https://cdnjs.cloudflare.com"
     )
-    .render('login', {
-      title: 'Log into your account',
+    .render("login", {
+      title: "Log into your account",
     });
 };
