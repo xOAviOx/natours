ac85eea1f5f1a8f5b95f35c69d2c0b0e8dccd252
xOAviOx <avishuklacode@gmail.com>
2025-05-05 19:56:18 +0530

    Tour images and image cover uploading functionality

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const tourController = require('./../controllers/tourController');
-const authController = require('./../controllers/authController');
-const reviewRouter = require('./../routes/reviewRoutes');
+const express = require("express");
+const tourController = require("./../controllers/tourController");
+const authController = require("./../controllers/authController");
+const reviewRouter = require("./../routes/reviewRoutes");
 const router = express.Router();
 
 // router.param('id', tourController.checkId);
@@ -14,46 +14,48 @@ const router = express.Router();
 //     reviewController.createReview
 //   );
 
-router.use('/:tourId/reviews', reviewRouter);
+router.use("/:tourId/reviews", reviewRouter);
 
 router
-  .route('/top-5-cheap')
+  .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats);
+router.route("/tour-stats").get(tourController.getTourStats);
 router
-  .route('/monthly-plan/:year')
+  .route("/monthly-plan/:year")
   .get(
     authController.protect,
-    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    authController.restrictTo("admin", "lead-guide", "guide"),
     tourController.getMonthlyPlan
   );
 
 router
-  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .route("/tours-within/:distance/center/:latlng/unit/:unit")
   .get(tourController.getToursWithin);
 
-router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+router.route("/distances/:latlng/unit/:unit").get(tourController.getDistances);
 router
-  .route('/')
+  .route("/")
   .get(tourController.getAllTours)
   .post(
     authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo("admin", "lead-guide"),
     tourController.createTour
   );
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(tourController.getTour)
   .patch(
     authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo("admin", "lead-guide"),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour
   )
   .delete(
     authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour
   );
 
